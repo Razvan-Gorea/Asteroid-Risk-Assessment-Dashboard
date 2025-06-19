839073139250600cb365b8aae95f7ffe0e63fe7c
Razvan Gorea <103372955+Razvan-Gorea@users.noreply.github.com>
2025-06-19 16:12:59 +0100

    UI progress

--- frontend/asteroid_app/src/App.jsx
+++ frontend/asteroid_app/src/App.jsx
@@ -1,14 +1,18 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
 
 function App() {
 
   return (
-    <>
+  <Router>
       <div>
-        <h1 class="text-3xl font-bold">    Hello world!  </h1>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+        </Routes>
       </div>
-    </>
-  )
+    </Router>
+  );
 }
 
 export default App
---
+++ frontend/asteroid_app/src/pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+function Dashboard(){
+    return (
+        <>
+        <div>
+            <h1 className="text-7xl">Come back later.. </h1>
+        </div>
+        </>
+    );
+}
+
+export default Dashboard; 
